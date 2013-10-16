b6f3bfecea2130af1e12a86c039f5ede60ee8298
Alessandro Parisi <ale@Alessandros-MacBook-Air.local>
2013-10-15 21:20:35 -0400

    Added tests to check if the constructor is storing the right values in Celsius. This is being done for values that are positive, negative, and not valid (<0K)

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -6,19 +6,35 @@
 
 import org.junit.* ;
 import static org.junit.Assert.* ;
+import org.junit.rules.ExpectedException;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
+	  // Add multiple tests to check all functions of
+	  // {@Code Temperature} class.
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
-
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+		//Testing the 2-input constructor if it is storing the values correctly (converting to and from kelvin)
+		
+		double precision = 0.1;
+		
+		@Rule
+		public ExpectedException exception = ExpectedException.none();
+		
+	     @Test
+	     public void test_constructor_taking2inputs_storingCelcius_positive (){
+	        System.out.println("Test if the constructor is storing the right values when taking celcius and result is positive");
+	        Temperature temp = new Temperature(40, Temperature.Units.CELSIUS);
+	        assertTrue(inPrecision(temp.getValue(), 40));
+	     }	
+	     @Test
+	     public void test_constructor_taking2inputs_storingCelcius_negative (){
+	        System.out.println("Test if the constructor is storing the right values when taking celcius and result is negative");
+	        Temperature temp = new Temperature(-10.5, Temperature.Units.CELSIUS);
+	        assertTrue(inPrecision(temp.getValue(), -10.5));
+	     }
+	     @Test (expected = IllegalArgumentException.class)
+	     public void test_constructor_taking2inputs_storingCelcius_pastBoundary (){
+	         System.out.println("Test if the constructor is storing the right value when taking celcius and result is not valid");
+	         Temperature temp = new Temperature(-280, Temperature.Units.CELSIUS);
+	      }
 }
